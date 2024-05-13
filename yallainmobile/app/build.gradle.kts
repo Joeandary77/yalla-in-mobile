48c00f00eaab2b290fdbb45a10ec91140ddeb8f4
Mario11h <mariohatoum7@gmail.com>
2024-05-13 18:15:57 +0300

    json

--- yallainmobile/app/build.gradle.kts
+++ yallainmobile/app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.yallain"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.yallain"
-        minSdk = 33
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -19,33 +19,31 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
         }
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildToolsVersion = "34.0.0"
-
-    // Enable Data Binding
-    buildFeatures {
-        dataBinding =true
-    }
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-firestore")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-storage:21.0.0")
-    implementation("com.google.firebase:firebase-firestore:25.0.0")
-    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.android.material:material:1.4.0") // Updated version
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3") // Updated version
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(kotlin("script-runtime"))
+    androidTestImplementation("androidx.test.ext:junit:1.1.3") // Updated version
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0") // Updated version
 }
