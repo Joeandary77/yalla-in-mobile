fc089ef4f7f75212b6b757c0a548e4ca2bf1c74a
joe <94402646+Joeandary77@users.noreply.github.com>
2024-05-13 14:53:19 +0300

    stad model + xml

---
+++ yallainmobile/app/src/main/java/com/example/yallain/StadiumModel.java
@@ -0,0 +1,12 @@
+package com.example.yallain;
+
+public class StadiumModel {
+    String name,photoUrl;
+    double price;
+
+    public StadiumModel(String name, String photoUrl, double price) {
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.price = price;
+    }
+}
